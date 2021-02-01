01bce74cc9158b62ec786b72d79ffeaadeaf6cf0
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2021-02-01 11:43:07 +0100

    updates folder structure
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

--- o3webapp/react-frontend/src/pages/Home/Home.js
+++ o3webapp/react-frontend/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 import './Home.css'
-import StartGeneratingButton from '../../components/buttons/StartGeneratingButton'
+import StartGeneratingButton from '../../components/buttons/StartGeneratingButton/StartGeneratingButton'
 
 function Home() {
     return (
