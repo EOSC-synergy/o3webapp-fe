990f986facd7efa817af07208e969911d816ae03
Arnolicious <arno.wiest@gmail.com>
2021-02-01 15:24:22 +0100

    Added style sheets for all the pages

---
+++ o3webapp/react-frontend/src/pages/About/About.js
@@ -0,0 +1,11 @@
+import './About.css'
+
+function About() {
+    return (
+        <div className="AboutPageContainer">
+            <p>About Page</p>
+        </div>
+    );
+}
+
+export default About;
--- o3webapp/react-frontend/src/pages/Manipulation.js
+++
@@ -1,9 +0,0 @@
-
-
-function Manipulation() {
-    return (
-        <p>Manipulation Page</p>
-    );
-}
-
-export default Manipulation;
