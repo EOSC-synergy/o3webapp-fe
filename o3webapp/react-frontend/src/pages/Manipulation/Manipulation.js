2805d9a7d4cb3c3e5e0adec821b12db8252e6942
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2021-02-08 14:22:37 +0100

    starts implementing the manipulation page
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

--- o3webapp/react-frontend/src/pages/Manipulation/Manipulation.js
+++ o3webapp/react-frontend/src/pages/Manipulation/Manipulation.js
@@ -1,11 +1,40 @@
-import './Manipulation.css'
+import React from 'react';
+import './Manipulation.css';
+import { embed } from '@bokeh/bokehjs';
 
-function Manipulation() {
-    return (
-        <div className="ManipulationPageContainer">
-            <p>Manipulation Page</p>
-        </div>
-    );
+const plot_api_url = 'http://localhost:8081/api/plot';
+
+class Manipulation extends React.Component {
+
+    componentDidMount() {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                //'Accept': 'application/json' 
+            },
+        };
+        // const response = await fetch(plot_api_url, requestOptions)
+        // const item = await response.json()
+        // embed.embed_item(item, 'test-plot')
+        console.log("calling api")
+        fetch(plot_api_url, requestOptions)
+            .then(response => response.json())
+            .then(data => embed.embed_item(data, 'test-plot'))
+            .then(console.log("called api"))
+    }
+
+
+    render() {
+        return (
+            <div className="ManipulationPageContainer">
+                <p>Manipulation Page</p>
+                <div id='test-plot' className="bk-root"></div>
+    
+            </div>
+        );
+    };
+    
 }
 
 export default Manipulation;
